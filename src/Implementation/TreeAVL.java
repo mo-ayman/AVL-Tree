ec78b7228ccf34159aa23e8593391642e25c8fb3
mo-ayman <m.ayman.saaed@gmail.com>
2022-03-05 12:14:32 +0200

    I've added search and delete

--- src/Implementation/TreeAVL.java
+++ src/Implementation/TreeAVL.java
@@ -28,6 +28,22 @@ public class TreeAVL {
         this.root = this.insert(newValue, this.root);
     }
 
+    private Node search(String value, Node node) {
+        if(node == null) return null;
+        if (node.getWord().equals(value)) return node;
+        int compareResult = node.getWord().compareToIgnoreCase(value);
+        if(compareResult > 0)
+            return search(value, node.getLeftChild());
+        else if(compareResult < 0)
+            return search(value, node.getRightChild());
+
+        return null;
+    }
+
+    public Node search(String value) {
+        return this.search(value, this.root);
+    }
+
     private void inOrder(Node node){
         if(node.hasLeftChild()) inOrder(node.getLeftChild());
         System.out.println("content: " + node.getWord());
@@ -37,10 +53,63 @@ public class TreeAVL {
         System.out.println("rightChild : " + node.hasRightChild());
         if(node.hasRightChild()) System.out.println("RightValue " + node.getRightChild().getWord());
         System.out.println();
-        if(node.hasRightChild()) inOrder(node.getRightChild());
+        if(node.hasRightChild())  inOrder(node.getRightChild());
     }
     public void inOrder(){
         inOrder(this.root);
+    }
+    private void preOrder(Node node) {
+        if(node == null) return;
+        System.out.print(node.getWord());
+        preOrder(node.getLeftChild());
+        preOrder(node.getRightChild());
+    }
+    public void preOrder(){
+        preOrder(this.root);
+    }
+
+
+
+    private Node replace(Node node) {
+            while (node.getLeftChild() != null) {
+                node = node.getLeftChild();
+            }
+            return node;
+
+    }
+
+    private Node delete(String value, Node node) {
+        if (node == null) return null;
+
+        int compareResult = node.getWord().compareToIgnoreCase(value);
+        if(compareResult > 0){
+            Node left = delete(value, node.getLeftChild());
+            node.setLeftChild(left);
+        }
+        else if(compareResult < 0){
+            Node right = delete(value, node.getRightChild());
+            node.setRightChild(right);
+        } else {
+            // node here to be deleted
+            if(node.hasRightChild()) {
+                Node aux = this.replace(node.getRightChild());
+                node.setWord(aux.getWord());
+                aux = delete(node.getWord(), node.getRightChild());
+                node.setRightChild(aux);
+            } else if (node.hasLeftChild()) {
+                node = node.getLeftChild();
+            } else {
+                    return null;
+            }
+
+        }
+        node.setHeight();
+        if(Math.abs(node.getBalance()) > 1) node = node.reBalance(node.getBalance());
+        return node;
+    }
+
+    public void delete(String value) {
+        this.root = this.delete(value, this.root);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import Implementation.TreeAVL;
-
+import Implementation.Node;
 public class Main {
     public static void main(String[] args){
 
         TreeAVL tree = new TreeAVL();
-        tree.insert("e");
-        tree.insert("b");
-        tree.insert("f");
-        tree.insert("d");
-        tree.insert("c");
-        tree.inOrder();
+        tree.insert("9");
+        tree.insert("1");
+        tree.insert("0");
+        tree.insert("1");
+        tree.insert("5");
+        tree.insert("2");
+        tree.insert("3");
+       // tree.delete("3");
+
+
+//        tree.preOrder();
+        Node node = tree.search("1");
+        if(node == null)
+            System.out.println("not found");
+//        tree.inOrder();
     }
 }
